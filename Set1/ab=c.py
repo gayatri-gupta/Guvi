33eb1798bcd0aa498fe63c88897e8624b33e2937
Gayatri Gupta <31720697+gayatri-gupta@users.noreply.github.com>
2019-03-15 01:08:37 +0530

    Create ab=c.py

---
+++ Set1/ab=c.py
@@ -0,0 +1,12 @@
+n = int(input())
+l = list(map(int,input().split()))
+
+for i in range(n-2):
+    
+    for j in range(i+1,n-1):
+        
+        for k in range(j+1,n):
+            
+            if(l[i]+l[j]==l[k]):
+                
+                print(l[i],l[j],l[k])
