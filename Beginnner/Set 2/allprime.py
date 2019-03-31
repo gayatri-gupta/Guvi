b089f7ff6bd6d431b1e61cf1d5e501cc774bc864
Gayatri Gupta <31720697+gayatri-gupta@users.noreply.github.com>
2019-03-31 21:17:57 +0530

    Create allprime.py

---
+++ Beginnner/Set 2/allprime.py
@@ -0,0 +1,12 @@
+l = list(map(int,input().split()))
+
+n = l[0]
+m = l[1]
+
+for i in range(n,m+1):
+    if i >1:
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            print(i,end=" ")
