2b196cd3dbfd64dce7341e493d9df13e107cf964
Gayatri Gupta <31720697+gayatri-gupta@users.noreply.github.com>
2019-04-19 02:06:52 +0530

    Create composite.py

---
+++ Beginner/Set 8/composite.py
@@ -0,0 +1,10 @@
+try:
+    n = int(input())
+    for i in range(2,n):
+        if n%i==0:
+            print("yes")
+            break
+    else:
+        print("no")
+except ValueError:
+    print("Invalid Input")
