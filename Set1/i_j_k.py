71fef471ac0c0535012a296b0cf19fa54309828d
Gayatri Gupta <31720697+gayatri-gupta@users.noreply.github.com>
2019-04-21 23:49:36 +0530

    Create i_j_k.py

---
+++ Set1/i_j_k.py
@@ -0,0 +1,21 @@
+
+try:
+    n =int(input())
+    l = list(map(int,input().split()))
+    
+    for i in range(0,n-2):
+        for j in range(i+1,n-1):
+            s = l[i] + l[j]
+            # print(s)
+            try:
+                    
+                if l.count(s) and l.index(s,j+1,n):
+                    print(l[i],l[j],s)
+            except:
+                pass
+            # for k in range(j+1,n):
+            # if l[i]+l[j] == l[k]:
+                # print(l[i],l[j],l[k])
+except:
+    print("Invalid Input")
+                    
